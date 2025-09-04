5f1c3d396b9e1f33490787c2a7ff979a246ae6f6
ron199807 <ronaldmweema@gmail.com>
2025-09-04 23:04:18 +0200

    Navigation using Stack Navigation Patterns

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -1,5 +1,5 @@
 import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
-import { styles } from "@/styles/_joinstyle";
+import { styles } from "@/styles/_join";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { ScrollView } from "react-native";
